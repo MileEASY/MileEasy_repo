c19361873a19a64ee698d6dd46d9b456bb1796bd
hyonie <dev.hyonie@gmail.com>
2022-12-27 21:24:44 +0900

    style: guestHome Update

--- controller/Crecommend.js
+++ controller/Crecommend.js
@@ -1,56 +1,58 @@
-const { User } = require('../model');
-const { Trip } = require('../model');
-const { Recommend } = require('../model');
+const { User } = require("../model");
+const { Trip } = require("../model");
+const { Recommend } = require("../model");
 
 //추천페이지
 exports.recommend_home = (req, res) => {
   if (req.session.user) {
-    res.render('recommend', {isLogin : true});
+    res.render("recommend", { isLogin: true });
+  } else {
+    res.render("recommend", { isLogin: false });
   }
-  else {
-    res.render('recommend', {isLogin : false});
-  }
-}
+};
 
 //사용자 추천페이지 보기
 exports.guest_home = async (req, res) => {
-  let result1 = await User.findOne({where : {id : req.session.user}});
-  let result2 = await Recommend.findAll();
-  if(req.session.user) {
-      res.render('guesthome', {isLogin: true, 
-        photo : result1.imgpath,
-        name : result2.name,
-        mbti : result2.mbti,
-        comment : result2.comment
-      });
-  } else res.render('guesthome', {isLogin: false});
-}
-
+  if (req.session.user) {
+    let result1 = await User.findOne({ where: { id: req.session.user } });
+    let result2 = await Recommend.findAll();
+    res.render("guesthome", {
+      isLogin: true,
+      photo: result1.imgpath,
+      name: result2.name,
+      mbti: result2.mbti,
+      comment: result2.comment,
+    });
+  } else {
+    let result2 = await Recommend.findAll();
+    res.render("guesthome", { isLogin: false });
+  }
+};
 //내 카드만 보기
 exports.mycard = async (req, res) => {
-  let result1 = await User.findOne({where : {id : req.session.user}});
-  let result2 = await Recommend.findAll({where : {name : req.session.user}});
+  let result1 = await User.findOne({ where: { id: req.session.user } });
+  let result2 = await Recommend.findAll({ where: { name: req.session.user } });
   res.send({
-    photo : result1.imgpath,
-    name : result2.name,
-    mbti : result2.mbti,
-    comment : result2.comment
-  })
-}
+    photo: result1.imgpath,
+    name: result2.name,
+    mbti: result2.mbti,
+    comment: result2.comment,
+  });
+};
 
 //여행지 추천 입력창
 exports.guest_post_home = async (req, res) => {
-  res.render('guest');
-}
+  res.render("guest");
+};
 
 //추천 여행지 입력 완료 버튼
 exports.cardcreate = async (req, res) => {
   let data = {
-      name : req.session.user,
-      // mbti: req.body.mbti,
-      comment : req.body.comment
-    }
-  let result = await Recommend.create(data)
-  console.log('post comment :', result);
+    name: req.session.user,
+    // mbti: req.body.mbti,
+    comment: req.body.comment,
+  };
+  let result = await Recommend.create(data);
+  console.log("post comment :", result);
   res.send(true);
-  }
+};
