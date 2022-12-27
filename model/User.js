95c5f7365ed7189228f9de0c82a5ed835ad8dd20
GSIII <kalseo901@gmail.com>
2022-12-27 20:58:04 +0900

    feat:result css change

--- model/User.js
+++ model/User.js
@@ -23,6 +23,7 @@ const User = (Sequelize, DataTypes)=>{
                 type: DataTypes.STRING(100),
                 allowNull: true,
                 defaulteValue: 'user_default_img.jpg',
+            }
         },
         {
             tableName: "User",
