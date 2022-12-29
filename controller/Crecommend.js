ad1855242ec372b13b0c8f4c524f5db0be76b35e
hyonie <dev.hyonie@gmail.com>
2022-12-29 12:26:09 +0900

    style: guestpage update

--- controller/Crecommend.js
+++ controller/Crecommend.js
@@ -17,13 +17,14 @@ exports.guest_home = async (req, res) => {
     let result2 = await Recommend.findAll();
     res.render("guesthome", {
       isLogin: true,
-      data : result2
+      data: result2,
     });
   } else {
     let result3 = await Recommend.findAll();
-    res.render("guesthome", { 
+    res.render("guesthome", {
       isLogin: false,
-      data : result3 });
+      data: result3,
+    });
   }
 };
 //내 카드만 보기
@@ -34,22 +35,24 @@ exports.mycard = async (req, res) => {
 
 //내 카드 삭제
 exports.card_delete = async (req, res) => {
-  console.log(req.body.rc_id)
-  let result1 = await Recommend.destroy({where : {rc_id : req.body.rc_id}});
+  console.log(req.body.rc_id);
+  let result1 = await Recommend.destroy({ where: { rc_id: req.body.rc_id } });
   console.log(result1);
-  let result2 = await Recommend.findAll({ where: { name: req.session.user }});
+  let result2 = await Recommend.findAll({ where: { name: req.session.user } });
   res.send(result2);
-}
+};
 
 //여행지 추천 입력창
 exports.guest_post_home = async (req, res) => {
-  let result = await User.findOne({where : {id : req.session.user}})
-  if (result.imgpath == 'user_default_img.jpg') {
-    res.render("guest", {data: result, photo: '/public/static/image/user_default_img.jpg'});
+  let result = await User.findOne({ where: { id: req.session.user } });
+  if (result.imgpath == "user_default_img.jpg") {
+    res.render("guest", {
+      data: result,
+      photo: "/public/static/image/user_default_img.jpg",
+    });
   } else {
-    res.render("guest", {data: result, photo : result.imgpath});
+    res.render("guest", { data: result, photo: result.imgpath });
   }
-  
 };
 
 //추천 여행지 입력 완료 버튼
@@ -59,7 +62,7 @@ exports.cardcreate = async (req, res) => {
     name: req.session.user,
     mbti: req.body.mbti,
     comment: req.body.comment,
-    imgpath : req.body.photo
+    imgpath: req.body.photo,
   };
   let result2 = await Recommend.create(data);
   console.log("post comment :", result2);
